496d54448e2bd1749c202882d7fad293aad6fc71
Precious <preciousmaregwa1997@gmail.com>
2022-04-17 11:25:07 +0300

    coplete assigment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const toDoItem = e.target["new-task-description"].value;
+
+    if (toDoItem.length > 0) {
+      publishToDo(toDoItem);
+    }
+  });
 });
+
+const publishToDo = (todo) => {
+  const tasks = document.querySelector("#tasks");
+  const paragraph = document.createElement("li");
+  tasks.appendChild(paragraph);
+  paragraph.textContent = `${todo} `;
+
+  const cancelBtn = document.createElement("button");
+  paragraph.appendChild(cancelBtn);
+  cancelBtn.textContent = "X";
+  cancelBtn.addEventListener("click", removeToDo);
+};
+
+const removeToDo = (e) => {
+  e.target.parentNode.remove();
+};
